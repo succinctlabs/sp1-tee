a4341bee59a55a1e6489e25a24bb9ba92d19cfa3
nhtyy <n@push0.dev>
2025-02-21 14:54:00 -0800

    uncomment main

--- enclave/src/main.rs
+++ enclave/src/main.rs
@@ -16,44 +16,38 @@ pub const VSOCK_PORT: u32 = 5005;
 pub const HOST_CID: u32 = 3;
 
 fn main() {
+    unsafe { ffi::aws_nitro_enclaves_library_init(std::ptr::null_mut()); }
+
+    println!("Hello, world!");
+
+    // Accept connections from any CID, on port `VSOCK_PORT`.
+    let listener = VsockListener::bind_with_cid_port(HOST_CID, VSOCK_PORT).unwrap();
+
     loop {
-        println!("Hello, world!");
+        let (stream, addr) = listener.accept().unwrap();
+        println!("Accepted connection from {:?}", addr);
+
+        let mut stream = BufReader::new(stream);
+
+        std::thread::spawn(move || {
+            handle_connection(&mut stream);
+        });
     }
 }
 
-// fn main() {
-//     unsafe { ffi::aws_nitro_enclaves_library_init(std::ptr::null_mut()); }
+fn handle_connection(stream: &mut BufReader<VsockStream>) {
+    let mut buf = [0; 1024];
 
-//     println!("Hello, world!");
+    loop {
+        let n = stream.read(&mut buf).unwrap();
 
-//     // Accept connections from any CID, on port `VSOCK_PORT`.
-//     let listener = VsockListener::bind_with_cid_port(HOST_CID, VSOCK_PORT).unwrap();
-
-//     loop {
-//         let (stream, addr) = listener.accept().unwrap();
-//         println!("Accepted connection from {:?}", addr);
-
-//         let mut stream = BufReader::new(stream);
-
-//         std::thread::spawn(move || {
-//             handle_connection(&mut stream);
-//         });
-//     }
-// }
-
-// fn handle_connection(stream: &mut BufReader<VsockStream>) {
-//     let mut buf = [0; 1024];
-
-//     loop {
-//         let n = stream.read(&mut buf).unwrap();
-
-//         if n == 0 {
-//             println!("Connection closed");
-//             break;
-//         }
+        if n == 0 {
+            println!("Connection closed");
+            break;
+        }
     
-//         println!("Received message: {:?}", String::from_utf8_lossy(&buf[..n]));
+        println!("Received message: {:?}", String::from_utf8_lossy(&buf[..n]));
         
-//         buf = [0; 1024];
-//     }
-// }
+        buf = [0; 1024];
+    }
+}

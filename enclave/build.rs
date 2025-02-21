a9b2cd2181faa219073c152477edae200d608d2b
nhtyy <n@push0.dev>
2025-02-20 18:01:10 -0800

    link everything

--- enclave/build.rs
+++ enclave/build.rs
@@ -4,6 +4,10 @@ fn main() {
     // #[cfg(not(target_os = "macos"))]
     let dst = cmake::build("aws-nitro-enclaves-sdk-c");
 
-    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-search=native={}/lib64", dst.display());
+    println!("cargo:rustc-link-lib=static=aws-c-auth");
+    println!("cargo:rustc-link-lib=static=aws-c-http");
+    println!("cargo:rustc-link-lib=static=aws-c-io");
+    println!("cargo:rustc-link-lib=static=aws-c-common");
     println!("cargo:rustc-link-lib=static=aws-nitro-enclaves-sdk-c");
 }

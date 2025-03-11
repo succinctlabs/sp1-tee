e14eef94741e947d0b0383275084b9c2589d9150
nhtyy <n@push0.dev>
2025-03-10 17:33:51 -0700

    fix(proxy): idemptonet setup

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -13,4 +13,7 @@ sudo cp ../proxy.nginx.conf /etc/nginx/nginx.conf
 # Ensure the service is enabled and started.
 sudo systemctl enable --now nginx
 
+# Restart the service for idempotency.
+sudo systemctl restart nginx
+
 popd

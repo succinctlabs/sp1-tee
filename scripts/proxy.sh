df5ed4c2d7d812c6ebb217a16beeabd84b640f54
nhtyy <n@push0.dev>
2025-03-10 17:24:54 -0700

    fix(proxy): sse optimizations

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -4,19 +4,13 @@ pushd $(dirname $0)
 # Install nginx.
 sudo dnf install -y nginx
 
-# Ensure the service is enabled and started.
-sudo systemctl enable --now nginx
-
 # Create the nginx directory if it doesn't exist.
 sudo mkdir -p /usr/local/nginx
 
 # Copy the nginx config.
 sudo cp ../proxy.nginx.conf /etc/nginx/nginx.conf
 
-# Reload the nginx config.
-sudo nginx -s reload
-
-# Start the server
-sudo nginx 
+# Ensure the service is enabled and started.
+sudo systemctl enable --now nginx
 
 popd

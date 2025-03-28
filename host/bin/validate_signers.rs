a7d8ca1d9a84b940f9f012a9c947b020d0589165
nhtyy <n@push0.dev>
2025-03-27 18:50:25 -0700

    fix: rm all functionality

--- host/bin/validate_signers.rs
+++ host/bin/validate_signers.rs
@@ -1,6 +1,5 @@
 use alloy::primitives::Address;
 use clap::{Parser, Subcommand};
-use sp1_tee_host::attestations::RawAttestation;
 
 #[derive(Parser)]
 struct Args {
@@ -14,8 +13,10 @@ enum Command {
     /// Validate a single signer.
     Signer {
         /// The signer to validate.
+        #[clap(long)]
         signer: Address,
         /// The PCR0 value to validate the signer against.
+        #[clap(long)]
         pcr0: String,
     },
     /// Validate all signers listed on a TEE verifier contract.
@@ -23,15 +24,12 @@ enum Command {
         /// The address of the contract to validate the signers of.
         contract: Address,
         /// The RPC URL to use to validate the signers.
+        #[clap(long)]
         rpc_url: String,
         /// The PCR0 value to validate the signers against.
+        #[clap(long)]
         pcr0: String,
-    },
-    /// Validate all signers that are attested in the s3 bucket.
-    All {
-        /// The PCR0 value to validate the signers against.
-        pcr0: String,
-    },
+    }
 }
 
 #[tokio::main]
@@ -66,61 +64,6 @@ async fn main() {
                     .unwrap();
 
                 println!("Validated signer: {:?}", signer);
-            }
-        }
-        Command::All { pcr0 } => {
-            let attestations = sp1_tee_host::attestations::get_raw_attestations()
-                .await
-                .expect("Failed to get attestations");
-
-            // For each attestation, verify the attestation and add the signer, optionally checking the PCR0.
-            for RawAttestation {
-                address,
-                attestation,
-            } in attestations
-            {
-                println!("-----------------------------------");
-
-                // Verify the attestation.
-                let doc = match sp1_tee_host::attestations::verify_attestation(&attestation) {
-                    Ok(doc) => doc,
-                    Err(e) => {
-                        eprintln!(
-                            "Failed to verify attestation for address: {:?}, error: {:?}",
-                            address, e
-                        );
-                        eprintln!("Its possible this can happen if an enclave goes down, and the expiry period has not been reached yet.");
-                        eprintln!("");
-                        continue;
-                    }
-                };
-
-                // Verify the PCR0 value.
-                let doc_pcr0 = hex::encode(doc.pcrs[&0].as_ref());
-                if doc_pcr0 != pcr0.replace("0x", "") {
-                    eprintln!(
-                        "PCR0 mismatch for address: {}, expected: {:?}, got: {:?}",
-                        address, pcr0, doc_pcr0
-                    );
-                    continue;
-                }
-
-                // Derive the address from the public key.
-                let pubkey_bytes = doc
-                    .public_key
-                    .expect("Public key is not set in attestation");
-
-                let derived_address = sp1_tee_host::ethereum_address_from_sec1_bytes(&pubkey_bytes)
-                    .expect("Failed to derive address from public key");
-
-                if derived_address != address {
-                    panic!(
-                        "Address mismatch expected: {:?}, got: {:?}",
-                        address, derived_address
-                    );
-                }
-
-                println!("Validated signer: {:?}", address);
             }
         }
     }

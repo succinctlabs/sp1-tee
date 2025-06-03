1c42351defbf62f72a5c6579d280074db524e48a
leruaa <aurelien.catinon@gmail.com>
2025-06-03 06:51:50 -0700

    nit: fmt

--- enclave/build.rs
+++ enclave/build.rs
@@ -6,7 +6,7 @@ fn main() {
             .cflag("-Wno-error=maybe-uninitialized")
             .build();
         println!("cargo:rustc-link-search=native={}/lib64", dst.display());
-        
+
         // The top level lib.
         println!("cargo:rustc-link-lib=static=aws-nitro-enclaves-sdk-c");
 
@@ -14,13 +14,13 @@ fn main() {
         println!("cargo:rustc-link-lib=static=aws-c-auth");
         println!("cargo:rustc-link-lib=static=aws-c-http");
         println!("cargo:rustc-link-lib=static=aws-c-io");
-    
+
         // c-auth/c-http also use c-compression, c-sdkutils, c-cal, and c-common
         println!("cargo:rustc-link-lib=static=aws-c-compression");
         println!("cargo:rustc-link-lib=static=aws-c-sdkutils");
         println!("cargo:rustc-link-lib=static=aws-c-cal");
         println!("cargo:rustc-link-lib=static=aws-c-common");
-    
+
         // s2n depends on libcrypto, so s2n should be linked before crypto
         println!("cargo:rustc-link-lib=static=s2n");
         println!("cargo:rustc-link-lib=static=crypto");
